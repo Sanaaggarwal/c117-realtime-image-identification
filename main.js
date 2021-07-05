dbfb5feac7fe19cca963d329cd36853751a4e29b
Sanaaggarwal <72506943+Sanaaggarwal@users.noreply.github.com>
2021-07-05 18:54:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,24 @@ function preload(){
 function setup(){
     canvas=createCanvas(400,300);
     canvas.center();
-    
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/4Ey0ONPPj/model.json",modelloaded);
+}
+function modelloaded(){
+    console.log("modelloaded");
+}
+function draw(){
+   image(video,0,0,400,300);
+   classifier.classify(video,getresult);
+}
+function getresult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object").innerHTML=results[0].label;
+        document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
